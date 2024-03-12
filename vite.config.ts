3a173b70496d53d2af144058859f39c1b09d5742
Tanya Madaan <tanyamadaan@Tanyas-MacBook-Air.local>
2024-03-12 22:57:45 +0530

    git pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/App-Check-Demo/",
+  base: "/qr-code-generator/",
   plugins: [react()],
 })
